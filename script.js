60d1bfaf1602f63b841bfce1ecbe421749f11db2
Christian Mills <millsy4@hotmail.com>
2020-10-01 21:41:37 -0500

    added more comments to the code

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ var passwordComplete = "";
 
 // Generate a password to use for the #password input
 function generatePassword() {
+
   // Create variables that will hold the arrays of the possible inputs for
   // the different values within the password
   var lowerAlphas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
@@ -26,6 +27,7 @@ function generatePassword() {
                       ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@',
                       '[', ']', '^', '_', '`', '{', '}', '|'];
   var allNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
   // Create variables that will be used to hold the information from
   // the questions that will be asked
   var lowerQuestion = false;
@@ -33,6 +35,7 @@ function generatePassword() {
   var numberQuestion = false;
   var specialQuestion = false;
   var passwordLength = 0;
+
   // Create variables that will be used to "count" within if else statements
   // to confirm that the certain necessary criteria will be met
   var lowerAlphasCounter = 0;
@@ -41,6 +44,8 @@ function generatePassword() {
   var specialCounter = 0;
   var k = 0;
 
+  // Initialize asking the questions with prompts and confirms and store the answers in variables,
+  // convert passwordLength to a number
   for (var i = 0; i < 1; i++) {
     lowerQuestion = confirm('Would you like lowercase letters? Okay for yes, cancel for no.');
     upperQuestion = confirm('Would you like uppercase letters? Okay for yes, cancel for no.');
@@ -49,6 +54,7 @@ function generatePassword() {
     passwordLength = prompt('How long would you like your password? (8+ characters)');
     passwordLength = Number(passwordLength);
 
+    // Test the validity of the answers (correct password length and at least one question must be true)
     if ((passwordLength < 8 || passwordLength > 128) && (lowerQuestion == false && upperQuestion == false && numberQuestion == false && specialQuestion == false)) {
       alert('Your password must be at least 8 to 128 characters AND you must select at least one password requirement.');
       lowerQuestion = false;
@@ -59,6 +65,7 @@ function generatePassword() {
       writePassword();
     }
 
+    // Test the validity of the answers (correct password length and at least one question must be true)
     if ((passwordLength < 8 || passwordLength > 128) && (lowerQuestion == true || upperQuestion == true || numberQuestion == true || specialQuestion == true)) {
       alert('You must enter a password length between 8 and 128!');
       lowerQuestion = false;
@@ -69,6 +76,7 @@ function generatePassword() {
       generatePassword();
     }
 
+    // Test the validity of the answers (correct password length and at least one question must be true)
     if ((passwordLength >= 8 && passwordLength <= 128) && (lowerQuestion == false && upperQuestion == false && numberQuestion == false && specialQuestion == false)) {
       alert('You must select at least one of the password requirements!');
       lowerQuestion = false;
@@ -79,6 +87,8 @@ function generatePassword() {
       writePassword();
     }
 
+    // Test to see if passwordLength is a number, this prevents people from entering words/special characters
+    // into the passwordLength variable that would break the code
     if (isNaN(passwordLength)) {
       alert('Password length must be a number!')
       lowerQuestion = false;
@@ -89,9 +99,19 @@ function generatePassword() {
       writePassword();
     }
 
+    // Test to ensure that the user information entered is valid
     if ((passwordLength >= 8 && passwordLength <= 128) && (lowerQuestion == true || upperQuestion == true || numberQuestion == true || specialQuestion == true)) {
+      
+      // If all the entered parameters are viable start inputting information into a string that will hold the password
+      // depending on which variables the user wanted
       if (lowerQuestion == true && upperQuestion == true && numberQuestion == true && specialQuestion == true) {
+
+        // Create a loop that will run according to how long the user input for the password length is
         for (var j = 0; j < passwordLength; j++){
+
+          // Create a variable that holds a random number that will be used to randomize which array containing
+          // password information will be accessed to give either an uppercase letter, lowercase letter, number
+          // or a special character
           var num = Math.floor(Math.random() * 4);
           if (num == 0) {
             var lowerNum = 0;
@@ -121,6 +141,11 @@ function generatePassword() {
             specialCounter++;
             k++;
           }
+
+          // Through the above if statements you use a variable count to see if the program accesses the information within,
+          // after it goes through the entire for loop, then it will check to make sure the different counters reached at least
+          // 1, and if they didn't, it will reset all the variables and redo the for loop.  This ensures that there is at least
+          // one of each character in the password
           if ((k == passwordLength) && (lowerAlphasCounter < 1 || upperAlphasCounter < 1 || numbersCounter < 1 || specialCounter < 1)) {
             j = -1;
             k = 0;
@@ -429,6 +454,8 @@ function generatePassword() {
       }
     }
   }
+
+  // return the passwordComplete variable so it can be passed to the writePassword() function
   return passwordComplete;
 }
 // Add event listener to generate button
